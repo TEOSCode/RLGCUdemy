ec7afdbd562333c3cc0bb4e403205131e58b2903
Lemus <lemusangel1092@hotmail.com>
2021-11-09 15:52:27 -0600

    Termine el fomrulario con los styled components, vamos a la mitad creo yo

--- cotizador/src/components/Formulario.js
+++ cotizador/src/components/Formulario.js
@@ -1,18 +1,76 @@
+import {useState} from 'react';
+import styled from '@emotion/styled';
+
+const Campo = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+  -webkit-appearance: none;
+`;
+
+const Radio = styled.input`
+  margin: 0 5px;
+`;
+
+const Boton = styled.button`
+  background-color: #00838f;
+  font-size: 16px;
+  width: 100%;
+  padding: 1rem;
+  color: #fff;
+  font-weight: bold;
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.2s ease;
+  margin-top: 12px;
+
+  &:hover {
+    background-color: #26c6da;
+  }
+`;
 const Formulario = () => {
+  const [datos, guardarDatos] = useState({
+    marca: '',
+    year: '',
+    plan: '',
+  });
+
+  //extraer valores del state
+  const {marca, year, plan} = datos;
+
+  //Leer los datos del formulario y colocarlos en el state
+  const obtenerDatos = e => {
+    guardarDatos({
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <form>
-      <div>
-        <label htmlFor="marca">Marca</label>
-        <select name="marca">
+      <Campo>
+        <Label htmlFor="marca">Marca: </Label>
+        <Select name="marca" id="marca" value={marca} onChange={obtenerDatos}>
           <option value="">--Seleccione--</option>
           <option value="Americano">Americano</option>
           <option value="Europeo">Europeo</option>
           <option value="Asiatico">Asiatico</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="año">Año</label>
-        <select name="año">
+        </Select>
+      </Campo>
+      <Campo>
+        <Label htmlFor="year">Año: </Label>
+        <Select name="year" id="year" value={year} onChange={obtenerDatos}>
           <option value="">-- Seleccione --</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
@@ -24,15 +82,32 @@ const Formulario = () => {
           <option value="2014">2014</option>
           <option value="2013">2013</option>
           <option value="2012">2012</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="plan">Plan:</label>
-        <input type="radio" name="plan" value="Basico" />
-        Basico
-        <input type="radio" name="plan" value="Completo" />
-        Completo
-      </div>
+        </Select>
+      </Campo>
+      <Campo>
+        <Label>Plan:</Label>
+        <Radio
+          type="radio"
+          id="plan1"
+          name="plan"
+          value="basico"
+          checked={plan === 'basico'}
+          onChange={obtenerDatos}
+        />
+        <Label htmlFor="plan1">Basico</Label>
+        <Radio
+          type="radio"
+          id="plan"
+          name="plan"
+          value="completo"
+          checked={plan === 'completo'}
+          onChange={obtenerDatos}
+        />
+        <Label htmlFor="plan">Completo</Label>
+      </Campo>
+      <Boton type="button" className="">
+        Cotizar
+      </Boton>
     </form>
   );
 };
